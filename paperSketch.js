42c2439ceaddd35196cedf98739356129d8e3cfd
Orr Kislev <orrkislev@gmail.com>
2022-06-23 22:39:23 +0300

    .

--- paperSketch.js
+++ paperSketch.js
@@ -18,7 +18,7 @@ const withColor = true
 const withShadow = withFloor && random() < 0.7
 const withLips = random()<0.2
 const withFace = true
-const doubleFace = random()<0.1
+const doubleFace = random()<0.05
 const tripleFace = doubleFace && random()<0.1
 
 function makeImage() {
@@ -62,7 +62,7 @@ function makeImage() {
     if (withFace) new Face()
     if (doubleFace) new Face()
     if (tripleFace) new Face()
-    
+
     if (withMoss) makeMoss()
     drawRocks()
 
